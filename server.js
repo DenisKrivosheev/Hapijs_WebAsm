c4320aa781677be8974f0de972f6a74c4af5765c
Epick <42617421+DenisKrivosheev@users.noreply.github.com>
2023-07-30 21:53:33 +0300

    add testing files (why im here?...)

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const init = async () => {
         port: 3000,
         host: 'localhost'
     });
-
+ 
     server.route({
         method: 'GET',
         path: '/',
@@ -22,6 +22,34 @@ const init = async () => {
             return h.response(htmlContent).type('text/html');
         }
     });
+
+    server.route({
+        method: 'GET',
+        path: '/march.wasm', // Define the route for the wasm file
+        handler: (request, h) => {
+            // Read the WebAssembly file asynchronously
+            const wasmFilePath = path.join(__dirname, 'march.wasm');
+            const wasmContent = fs.readFileSync(wasmFilePath);
+
+            return h.response(wasmContent).type('application/wasm');
+        }
+    });
+
+
+    server.route({
+        method: 'GET',
+        path: '/march.js', // Define the route for the JavaScript glue code file
+        handler: (request, h) => {
+            // Read the JavaScript glue code file asynchronously
+            const jsFilePath = path.join(__dirname, 'march.js');
+            const jsContent = fs.readFileSync(jsFilePath, 'utf8');
+
+            return h.response(jsContent).type('application/javascript');
+        }
+    });
+
+
+
 
     await server.start();
     console.log('Server running on %s', server.info.uri);
